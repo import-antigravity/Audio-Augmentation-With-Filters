46a3f4358bc8cf2e6ed5e8f0df975008164ac221
Robbie Dozier <importantigrav@gmail.com>
2019-12-14 02:48:46 -0500

    Added conform_examples method to conform audio clips to windows of the same length

---
+++ audio-augmentation/data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def conform_examples(X_list: [np.ndarray], y: np.ndarray, window_size: int, crossover: float):
+    assert len(X_list) == y.size
+
+    X = []
+    y = []
+
+    for i in range(len(X_list)):
+        sample = X_list[i]
+        if sample.size <= window_size:
+            zeros = np.zeros(window_size - sample.size)
+            padded = np.concatenate((sample, zeros))
+            X.append(padded)
+            y.append(y[i])
+        else:
+            current_start = 0
+            while current_start < sample.size:
+                zeros = np.array([])
+                if current_start + window_size > sample.size:
+                    zeros = np.zeros(current_start + window_size - sample.size)
+                padded = np.concatenate((sample[current_start:current_start + window_size], zeros))
+                X.append(padded)
+                y.append(y[i])
+                current_start += int((1. - crossover) * window_size)
+
+    return np.vstack(X), np.vstack(y)
---
+++ audio-augmentation/models.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+
+def cnn_rand():
+    cnn = [
+        tf.keras.Input(shape=509999),
+        tf.keras.layers.Reshape((50999, 1)),
+        tf.keras.layers.Conv1D(16, 64, strides=2, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool1D(pool_size=8, strides=8),
+        tf.keras.layers.Conv1D(32, 32, strides=2, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool1D(pool_size=8, strides=8),
+        tf.keras.layers.Conv1D(128, 8, strides=2, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv1D(256, 4, strides=2, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool1D(pool_size=4, strides=4),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax'),
+    ]
+
+    return tf.keras.Sequential(cnn)
