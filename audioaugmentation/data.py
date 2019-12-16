a69bd47e816e601b4ac3e1da60d5f095909bb996
Philip Woolley <philipgmwoolley@gmail.com>
2019-12-16 14:10:08 -0500

    fixing the data augmentation method

--- audioaugmentation/data.py
+++ audioaugmentation/data.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 import numpy as np
 import tensorflow as tf
 import pickle
@@ -47,7 +49,7 @@ def import_numpy(data_path: str, feature_size: int):
 
 
 def import_augmented_data(augmentation_percent: float, noise_mean: float, noise_stddev: float, num_rooms: int):
-    with open(f'..{os.sep}data{os.sep}UrbanSound_sr16000.dms', 'rb') as fp:
+    with open(f'..{os.sep}data{os.sep}UrbanSound_sr16000', 'rb') as fp:
         itemlist = pickle.load(fp)
     labels = itemlist.pop('class_label')
     le = LabelEncoder()
@@ -57,18 +59,24 @@ def import_augmented_data(augmentation_percent: float, noise_mean: float, noise_
     nd = noise_distribution(noise_mean, noise_stddev)
     rd = room_distribution(num_rooms)
     test_set_size = int(0.2 * features.shape[0])
-    test_features = features[:test_set_size]
-    train_features = features[test_set_size:]
-    test_labels = labels[:test_set_size]
-    train_labels = labels[test_set_size:]
-    for f in [rd.augment, nd.augment]:
-        test_features.map(lambda x: tf.cond(tf.random_uniform([], 0, 1) > (1 - augmentation_percent), lambda: f(x), lambda: x))
-        train_features.map(lambda x: tf.cond(tf.random_uniform([], 0, 1) > (1 - augmentation_percent), lambda: f(x), lambda: x))
+    test_features = np.asarray(features[:test_set_size])
+    train_features = np.asarray(features[test_set_size:])
+    test_labels = np.asarray(labels[:test_set_size])
+    train_labels = np.asarray(labels[test_set_size:])
+    X = list(train_features)
+    y=train_labels
+    for i in range(train_features.shape[0]):
+        if tf.random.uniform(shape=[1, 1]) < augmentation_percent:
+            temp = np.asarray(rd.augment(train_features[i]))
+            X.append(temp)
+            y = np.append(y, np.reshape(train_labels[i, :], newshape=[10, 1]).T, axis=0)
+    train_features = np.asarray(X)
+    train_labels = y
+    for i in range(test_features.shape[0]):
+        pass
     test_features, test_labels = conform_examples(list(test_features), test_labels, 50999, 0.5)
     train_features, train_labels = conform_examples(list(train_features), train_labels, 50999, 0.5)
-    Testing_Dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels))
-    Training_Dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
-    return Training_Dataset, Testing_Dataset
+    return test_features, test_labels, train_features, train_labels
 
 
 def conform_examples(X_list: [np.ndarray], y_original: np.ndarray, window_size: int, crossover: float):
@@ -115,8 +123,7 @@ class room_distribution(object):
         # TODO: create a distribution of PRA rooms
         for i in range(num_rooms):
             dims = np.random.randint(low=1, high=10, size=2)
-            room = pra.ShoeBox(dims, fs=16000)
-            self.rooms.append(room)
+            self.rooms.append(dims)
 
     @staticmethod
     def sample_loc(walls):
@@ -148,7 +155,8 @@ class room_distribution(object):
     # returns a room populated with a source and microphone array, drawn from the random distributions
     def sample(self):
         # TODO: add a random source and microphone to a random room, then return
-        room = np.random.choice(self.rooms)
+        dims = random.choice(self.rooms)
+        room = pra.ShoeBox(dims, fs=16000)
         source = room_distribution.sample_source(room)
         mic = room_distribution.sample_mic(room)
         room.add_source(source)
@@ -158,13 +166,9 @@ class room_distribution(object):
     def augment(self, x: tf.Tensor) -> tf.Tensor:
         room = self.sample()
         room.sources[0].add_signal(x)
-        room.simulate()
-        return room.mic_array.signals[1, :]
+        room.simulate(recompute_rir=True)
+        return room.mic_array.signals[0, :]
 
 
 if __name__ == "__main__":
-    print(np.array([2.4238, 4.43235]))
-    rd = room_distribution(100)
-    s = rd.sample()
-    s.plot()
-    plt.show()
+    a, b, c, d = import_augmented_data(0.25, 0, 1, 100)
