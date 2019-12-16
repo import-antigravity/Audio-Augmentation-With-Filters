8879f7d96cd9c8777193f4eff1007a5f8cced2be
Robbie Dozier <importantigrav@gmail.com>
2019-12-15 20:49:06 -0500

    train custom path

--- audioaugmentation/train.py
+++ audioaugmentation/train.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
+import os
 from audioaugmentation.data import import_numpy
 
 
-def train(classifier, optimizer, epochs, batch_size):
+def train(classifier, optimizer, epochs, batch_size, path):
     print('Loading dataset')
     test_features, test_labels, train_features, train_labels = import_numpy('../data')
     print(train_features.shape, train_labels.shape)
     classifier.compile(optimizer, loss=tf.keras.losses.binary_crossentropy)
     print('Training...')
-    checkpoint = tf.keras.callbacks.ModelCheckpoint('../models/{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss',
-                                                    verbose=0, save_best_only=True, save_weights_only=False,
-                                                    mode='auto', period=1)
-    csv_logger = tf.keras.callbacks.CSVLogger("../models/model_history_log.csv", append=True)
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(path + os.linesep + '{epoch:02d}-{val_loss:.2f}.hdf5',
+                                                    monitor='val_loss', verbose=0, save_best_only=True,
+                                                    save_weights_only=False, mode='auto', period=1)
+    csv_logger = tf.keras.callbacks.CSVLogger(path + os.linesep + "model_history_log.csv", append=True)
     history = classifier.fit(train_features, train_labels, validation_data=(test_features, test_labels),
                              batch_size=batch_size, epochs=epochs, callbacks=[checkpoint, csv_logger])
     return classifier, optimizer, history
