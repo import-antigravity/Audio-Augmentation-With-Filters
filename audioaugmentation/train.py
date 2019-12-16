4b53518b88162756a56630085fe948d388c3624d
Robbie Dozier <importantigrav@gmail.com>
2019-12-15 20:52:35 -0500

    wtf

--- audioaugmentation/train.py
+++ audioaugmentation/train.py
@@ -9,10 +9,10 @@ def train(classifier, optimizer, epochs, batch_size, path):
     print(train_features.shape, train_labels.shape)
     classifier.compile(optimizer, loss=tf.keras.losses.binary_crossentropy)
     print('Training...')
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(path + os.linesep + '{epoch:02d}-{val_loss:.2f}.hdf5',
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(path + '{epoch:02d}-{val_loss:.2f}.hdf5',
                                                     monitor='val_loss', verbose=0, save_best_only=True,
                                                     save_weights_only=False, mode='auto', period=1)
-    csv_logger = tf.keras.callbacks.CSVLogger(path + os.linesep + "model_history_log.csv", append=True)
+    csv_logger = tf.keras.callbacks.CSVLogger(path + "model_history_log.csv", append=True)
     history = classifier.fit(train_features, train_labels, validation_data=(test_features, test_labels),
                              batch_size=batch_size, epochs=epochs, callbacks=[checkpoint, csv_logger])
     return classifier, optimizer, history
