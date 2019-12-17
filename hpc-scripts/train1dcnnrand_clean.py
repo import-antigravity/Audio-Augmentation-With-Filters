3b0527b4a208d0c53d6cfc95d6a22a5a83209112
Robbie Dozier <importantigrav@gmail.com>
2019-12-17 02:52:45 -0500

    changed epochs to 500

--- hpc-scripts/train1dcnnrand_clean.py
+++ hpc-scripts/train1dcnnrand_clean.py
@@ -15,7 +15,7 @@ data = import_clean_data('../data/', 32000)
 
 classifier, optimizer, history = train(data, model,
                                        optimizer=tf.keras.optimizers.Adam(1e-3),
-                                       epochs=1000,
+                                       epochs=500,
                                        batch_size=10,
                                        path=str(sys.argv[2]),
                                        num_gpus=int(sys.argv[1]))
