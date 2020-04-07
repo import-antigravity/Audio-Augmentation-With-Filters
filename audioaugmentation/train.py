5ca63917c8b72275218af316ecf47e68e21c2bc3
Robbie Dozier <importantigrav@gmail.com>
2020-04-07 14:08:50 -0500

    added loss as parameter

--- audioaugmentation/train.py
+++ audioaugmentation/train.py
@@ -5,14 +5,13 @@ import numpy as np
 from tensorflow import keras
 
 
-def train_baseline(model, optimizer, name: str, num_epochs: int, batch_size: int,
+def train_baseline(model, optimizer, loss, name: str, num_epochs: int, batch_size: int,
                    data: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray], callbacks: list = None,
                    model_path: str = None):
     if model_path is None:
         model_path = f'../models/{name}'
     if callbacks is None:
         callbacks = [
-            keras.callbacks.ReduceLROnPlateau(min_lr=1e-12),
             keras.callbacks.ModelCheckpoint(os.path.join(model_path, '{epoch:02d}-{val_loss:.2f}.hdf5'),
                                             monitor='val_loss', verbose=0, save_best_only=True,
                                             save_weights_only=True, mode='auto', period=1),
@@ -26,7 +25,7 @@ def train_baseline(model, optimizer, name: str, num_epochs: int, batch_size: int
     os.makedirs(model_path, exist_ok=True)
 
     print('Dataset:', X_train.shape, y_train.shape)
-    model.compile(optimizer, loss=keras.losses.mean_squared_logarithmic_error)
+    model.compile(optimizer, loss=loss)
     print('Training...')
     history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=batch_size, epochs=num_epochs,
                         callbacks=callbacks)
